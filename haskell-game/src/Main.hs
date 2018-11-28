ed55a89293b3d6faa22e0f3a8702dd7dde7c9d68
backend-giraffe <jan.seb.eve@gmail.com>
2018-11-28 13:31:39 +0100

    unfinished game

--- haskell-game/src/Main.hs
+++ haskell-game/src/Main.hs
@@ -2,13 +2,13 @@ module Main where
 
 main :: IO ()
 main = do
-  buildMap "o" (20*20) [(0,0),(1,1),(2,2),(3,3)]
+  buildMap "." (20*20) [(2,0),(2,1),(2,2),(1,2),(0,1)]
 
 createField :: Int -> String -> Int -> [(Int, Int)] -> String
 createField number character rowLength coordinates
-  | number `mod` rowLength == 0 && number >= rowLength^2   = if(isInCoordinates coordinates number rowLength) then "x" ++ " │\n" else character ++ " │\n"
-  | number `mod` rowLength == 0                            = if(isInCoordinates coordinates number rowLength) then "x" ++ " │\n|" else character ++ " │\n|"
-  | otherwise                                              = if(isInCoordinates coordinates number rowLength) then "x" else character
+  | number `mod` rowLength == 0 && number >= rowLength^2   = if(isInCoordinates coordinates number rowLength) then "o" ++ " │\n" else character ++ " │\n"
+  | number `mod` rowLength == 0                            = if(isInCoordinates coordinates number rowLength) then "o" ++ " │\n│" else character ++ " │\n|"
+  | otherwise                                              = if(isInCoordinates coordinates number rowLength) then "o" else character
 
 integerSqrtFrom :: Int -> Int
 integerSqrtFrom x = floor . sqrt $ (fromIntegral x :: Float)
@@ -28,7 +28,17 @@ index pos rowSize = fst(pos)*rowSize + snd(pos) + 1
 isInCoordinates :: [(Int, Int)] -> Int -> Int -> Bool
 isInCoordinates coordinates number rowSize = elem number (map(\ x -> index x rowSize) coordinates)
 
--- TODOfillInCoordinates list coordinates = do
+-- TODO: create new generation
+nextGeneration :: [(Int,Int)] -> [(Int,Int)]
+nextGeneration oldgeneration = map (\ cell -> nextCell cell oldgeneration ) oldgeneration
+
+nextCell :: (Int,Int) -> [(Int,Int)] -> (Int,Int)
+nextCell cell previousCells
+  | overpopulation cell previousCells = (1,1)
+
+overpopulation :: (Int,Int) -> [(Int,Int)] -> bool
+overpopulation cell oldgeneration = 
+-- END
 
 buildMap character fieldSize coordinates = do
   let fields = [1..fieldSize]
